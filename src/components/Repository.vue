4a7438e5ebd427c67f5600ac2180fa9013594f48
Ledrunner <ledrunner@hotmail.com>
2022-01-24 15:48:32 +0300

    Added  project and vue component for repository

---
+++ src/components/Repository.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <section>
+      <div>
+        <div></div>
+      </div>
+    </section>
+  <div class="container">
+      <div class="table-container">
+          <table class="table is-bordered is-stripped is-hoverable is-fullwidth">
+
+          </table>
+      </div>
+  </div>
+  </div>
+</template>
